4b89537c57d19985441753efb17f1ded6bb1f96c
Laura Melo <lauramelos@gmail.com>
2012-06-22 10:37:59 -0300

    replace text by htext in twitters list

--- templates/fullsocial-twitter.php
+++ templates/fullsocial-twitter.php
@@ -1,12 +1,16 @@
 <div class="wp-fullsocial-widget-<?php echo $id; ?>">
-  <?php $twws = $data['twitts']; ?>
+<?php $twws = $data['twitts']; ?>
 <ul>
   <?php foreach($twws as $tws) : ?>
     <?php $results = $tws['results']; ?>
     <?php foreach($results as $tw) : ?>
       <li>
+        <?php
+        $t = $tw['text'];
+        $t = preg_replace('/(http\:\/\/[a-z,A-Z,0-9,_,-,\.,\/]+)/', '<a href="$1">$1</a>', $t);
+        ?>
         <img src="<?php echo $tw['profile_image_url']; ?>" />
-        <?php echo html_entity_decode($tw['text']); ?>
+        <?php echo html_entity_decode($t); ?>
       </li>
     <?php endforeach; ?>
   <?php endforeach; ?>
