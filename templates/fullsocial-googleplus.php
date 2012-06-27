8e49863f174f7a6c21b552e3f50b3a77b602aed5
Laura <lauramelos@gmail.com>
2012-06-27 12:34:36 -0300

    google plus template

--- templates/fullsocial-googleplus.php
+++ templates/fullsocial-googleplus.php
@@ -3,12 +3,18 @@
 ?>
 
 <div class="wp-fullsocial-widget-<?php echo $id; ?>">
+<!-- Place this tag where you want the badge to render. -->
+<div class="g-plus" data-width="260" data-href="https://plus.google.com/<?php echo $instance['googleplus_userid'] ?>?rel=publisher"></div>
+<!-- Place this render call where appropriate. -->
+<script type="text/javascript">gapi.plus.go();</script>
 <ul>
   <?php foreach($entries as $entry) : ?>
       <li>
-        <a href="<?php echo $entry['url']; ?>" target="_blank">
-          <?php echo html_entity_decode($entry['title']); ?>
-        </a>
+        <h5>
+          <a href="<?php echo $entry['url']; ?>" target="_blank">
+            <?php echo html_entity_decode($entry['title']); ?>
+          </a>
+        </h5>
       </li>
     <?php endforeach; ?>
 </ul>
